c6ab4f8d23ea52a45976a83d1cbcdf4fc0ec4792
YoussefIbraheem <youssefm.muhammed@gmail.com>
2025-09-24 22:33:13 +0300

    Refactor static and media settings; update STATIC_URL, add STATICFILES_DIRS, MEDIA_URL, and MEDIA_ROOT in settings.py, and configure URL patterns for media files in urls.py. Restore userCreatedAlert functionality in app.js.

--- edenthought/edenthought/settings.py
+++ edenthought/edenthought/settings.py
@@ -88,7 +88,13 @@ DATABASES = {
 }
 
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
